604308aa2ca5040207e46f444cd4f1a0bdedcb45
Matthias Schlagenhaufer <mathias_schlagenhaufer@reyrey.com>
2024-10-17 14:20:35 +0200

    todos sind jetzt anhacken

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Hi fellers
 # ------------------------------ Version ------------------------------#
 #Version 1.0.0
-
+import json
 # ------------------------------ Import ------------------------------#
 import sys
 #from lib2to3.btm_utils import MinNode
@@ -17,17 +17,31 @@ from PyQt6.QtCore import QTime, QTimer
 # ------------------------------ Standarts ------------------------------#
 
 
-#TODO: fehlermeldung gedönz zentrieren
+#
 
 
 
 # ------------------------------ Code ------------------------------#
+
 
 def main_setup():
     if not MainFenster.textEdit.toPlainText():
         with open("PData/notiz.txt") as file:
             if file:
                 MainFenster.textEdit.setText(file.read())
+
+    todo = ToDo()
+    todo.set_todos()
+
+    # bisschen hard dirty bitte besser machen
+    def chbo1():
+        QTimer.singleShot(550, lambda: todo.eins())
+
+    def chbo2():
+        QTimer.singleShot(550, lambda: todo.zwei())
+
+    def chbo3():
+        QTimer.singleShot(550, lambda: todo.drei())
 
     MainFenster.lineEdit.editingFinished.connect(do_search)
 
@@ -36,11 +50,14 @@ def main_setup():
     MainFenster.pushButton_3.clicked.connect(last_button)
     MainFenster.pushButton_4.clicked.connect(marked_button)
 
-    MainFenster.checkBox.stateChanged.connect(chbo.eins)
-    MainFenster.checkBox_2.stateChanged.connect(chbo.zwei)
-    MainFenster.checkBox_3.stateChanged.connect(chbo.drei)
+    MainFenster.checkBox.stateChanged.connect(chbo1)
+    MainFenster.checkBox_2.stateChanged.connect(chbo2)
+    MainFenster.checkBox_3.stateChanged.connect(chbo3)
 
     MainFenster.textEdit.textChanged.connect(save_note)
+
+
+    MainFenster.label.setText("")
 
 
 def save_note():
@@ -76,12 +93,8 @@ def do_search():
                 open_file(pfad)
 
         else:
-            def error_datei_nicht_gefunden():
-                MainFenster.label.setText("Datei nicht Vorhanden")
-                time.sleep(5)
-                MainFenster.label.clear()
-            timer_thread = threading.Thread(target=error_datei_nicht_gefunden)
-            timer_thread.start()
+            MainFenster.label.setText("Datei nicht Vorhanden")
+            QTimer.singleShot(1000, lambda:MainFenster.label.clear())
 
 def open_file(pfad):
     with open(pfad, "w") as file:
@@ -90,15 +103,71 @@ def open_file(pfad):
     pass
 
 
-# git test
 
-class chbo():
+class ToDo:
+    def __init__(self):
+        with open("PData/todos.json") as file:
+            self.datei = json.load(file)
+
+
     def eins(self):
-        print("chbo1")
+        if MainFenster.checkBox.isChecked():
+            self.datei.pop(0)
+            self.todo_done()
+            self.set_todos()
+            MainFenster.checkBox.setChecked(False)
+
     def zwei(self):
-        print("chbo2")
+        if MainFenster.checkBox_2.isChecked():
+            self.datei.pop(1)
+            self.todo_done()
+            self.set_todos()
+            MainFenster.checkBox.setChecked(False)
+
+
     def drei(self):
-        print("chbo3")
+        if MainFenster.checkBox_3.isChecked():
+            self.datei.pop(2)
+            self.todo_done()
+            self.set_todos()
+            MainFenster.checkBox.setChecked(False)
+
+
+
+    def todo_done(self):
+        with open("PData/todos.json", "w") as file:
+            json.dump(self.datei, file)
+
+
+
+
+
+    def set_todos(self):
+        if len(self.datei) >= 3:
+            MainFenster.checkBox.setText(self.datei[0])
+            MainFenster.checkBox_2.setText(self.datei[1])
+            MainFenster.checkBox_3.setText(self.datei[2])
+
+        elif len(self.datei) == 2:
+            MainFenster.checkBox.setVisible(True)
+            MainFenster.checkBox_2.setVisible(True)
+            MainFenster.checkBox_3.setVisible(False)
+
+            MainFenster.checkBox.setText(self.datei[0])
+            MainFenster.checkBox_2.setText(self.datei[1])
+
+        elif len(self.datei) == 1:
+            MainFenster.checkBox.setVisible(True)
+            MainFenster.checkBox_2.setVisible(False)
+            MainFenster.checkBox_3.setVisible(False)
+
+            MainFenster.checkBox.setText(self.datei[0])
+
+        elif len(self.datei) == 0:
+            MainFenster.checkBox.setVisible(False)
+            MainFenster.checkBox_2.setVisible(False)
+            MainFenster.checkBox_3.setVisible(False)
+
 # ------------------------------ Kontrolls ------------------------------#
 app = QtWidgets.QApplication(sys.argv)
 
@@ -110,13 +179,5 @@ MainFenster = ui()
 MainFenster.setupUi(window)
 main_setup()
 
-
-
-
-
-#################### System ####################
-
-
 window.show()
 sys.exit(app.exec())
-#################### END ####################
--- ui/mainwindow.py
+++ ui/mainwindow.py
@@ -75,7 +75,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "hias Note"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "hiasNote"))
         self.pushButton.setText(_translate("MainWindow", "New"))
         self.pushButton_2.setText(_translate("MainWindow", "Brows"))
         self.pushButton_3.setText(_translate("MainWindow", "Last"))
